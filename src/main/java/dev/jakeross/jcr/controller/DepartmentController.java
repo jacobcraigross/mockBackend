b9588d68b564cccf9e66aeae27875e079935e8c3
jacobcraigross <jakerossdev@gmail.com>
2022-04-27 11:32:44 -0500

    added logging to POST and GET mappings

--- src/main/java/dev/jakeross/jcr/controller/DepartmentController.java
+++ src/main/java/dev/jakeross/jcr/controller/DepartmentController.java
@@ -2,10 +2,14 @@ package dev.jakeross.jcr.controller;
 import dev.jakeross.jcr.entity.Department;
 import dev.jakeross.jcr.service.DepartmentService;
 import dev.jakeross.jcr.service.DepartmentServiceImplementation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
+
+// adding loggers ---------- 1:37:06 ----------
 
 
 @RestController
@@ -14,15 +18,19 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     // Post a Department to the DB
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("logging from inside saveDepartment POST mapping of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     // Get all Departments from the DB
     @GetMapping("/departments")
     public List <Department> fetchDepartmentList() {
+        LOGGER.info("logging from inside fetchDepartmentList GET mapping of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
